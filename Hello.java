4d0b3bd2189bb2f174b68fc99a549b8489ddc56b
Akash Kushwaha <akashkushwaha76986@gmail.com>
2025-03-04 16:21:34 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next());
+    }
+}
